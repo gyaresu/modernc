338bfb30fa92f91d1be7c4dafc6dc77c271dbbfa
Gareth <me@gareth.codes>
2015-09-08 12:21:39 -0700

    extra print functions and messing with formatting

--- 02.c
+++ 02.c
@@ -5,9 +5,10 @@
 #include <stdio.h> // Added this to define where printf comes from
 
 /* The main thing that this programme does. */
-int main(void) { // Changed to int from voic for return type
-  // Declarations 
+int main(void) { // Changed to int from void for return type
+  // Declarations
   int i;
+  //char c = "--- \n";
   double A[5] = {
     9.0,
     2.9,
@@ -17,10 +18,15 @@ int main(void) { // Changed to int from voic for return type
 
   // Doing some work
   for (i = 0; i < 5; ++i) {
-    printf("element %d is %g, \tits square is %g\n",
-      i,
-      A[i],
-      A[i]*A[i]);
+    printf("'i' equals %d\n\n", i);
+    if (i) {
+      printf("element %d is %g, \tits square is %g\n\n",
+          i,
+          A[i],
+          A[i]*A[i]
+      );
+    }
+    printf("   --- End itteration %d ---\n\n", i);
   }
 
   return 0;
